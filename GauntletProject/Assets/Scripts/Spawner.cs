d2224e1447b109c654da3b51492d83edfa9d9307
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2024-04-16 13:48:05 -0700

    Partially Finished the Spawner Script (Spawn Delay still WIP)

--- GauntletProject/Assets/Scripts/Spawner.cs
+++ GauntletProject/Assets/Scripts/Spawner.cs
@@ -8,23 +8,53 @@ public class Spawner : MonoBehaviour
     public int spawnLevel;
     public int spawnDir;
     public int dmgManipulator;
+    public float spawnTime = 1f;
 
-    public GameObject enemyPrefab;
+    public GameObject ghostPrefab;
+
+    public SpawnerType spawnType;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnTypeSetup();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnEnemy();
+    }
+
+    public enum SpawnerType
+    {
+        Ghost,
+
+    }
+
+    private void SpawnTypeSetup()
+    {
+        switch (spawnType)
+        {
+            case SpawnerType.Ghost:
+                spawnLevel = 0;
+                break;
+        }
+
     }
 
     private void SpawnEnemy()
     {
+        if (spawnLevel == 0)
+            {
+            StartCoroutine(GhostSpawnDelay());
+        }
+    }
+
+    public IEnumerator GhostSpawnDelay()
+    {
+        Instantiate(ghostPrefab, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(spawnTime);
 
     }
 
