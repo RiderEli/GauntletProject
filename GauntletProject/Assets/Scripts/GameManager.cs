53535fa87d55c355f2bc7b544cd36b0c38c77f94
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2024-04-18 13:46:58 -0700

    Added GameManager Script

---
+++ GauntletProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentScore;
+    public int highScore;
+    public int dungeonLevel;
+
+    public TMPro.TextMeshPro dialouge;
+
+    public GameObject itemPickUp;
+
+    private void Display()
+    {
+
+    }
+
+    private void Narrator() 
+    {
+    
+    }
+
+    private void SpawnDungeonLevel()
+    {
+
+    }
+
+    private void SpawnPickUpItem()
+    {
+
+    }
+}
