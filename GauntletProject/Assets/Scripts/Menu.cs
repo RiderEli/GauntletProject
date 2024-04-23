553653af527403ccee13b3bd7fd611a41950afa5
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2024-04-23 13:41:39 -0700

    Added Menu and a New Font Pack

---
+++ GauntletProject/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
