fde9f982e30fae5e06b662a7812ff8833bc104ec
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2024-04-25 13:31:38 -0700

    Extra Changes for the Player script and made the test spawner a prefab

--- GauntletProject/Assets/Scripts/Player.cs
+++ GauntletProject/Assets/Scripts/Player.cs
@@ -7,7 +7,8 @@ using static UnityEngine.InputSystem.DefaultInputActions;
 public class Player : MonoBehaviour
 {
     PlayerController controller;
-   // private Rigidbody playerRB;
+    private Rigidbody playerRB;
+    
 
     [SerializeField] protected float moveSpeed;
     public GameObject shot;
@@ -16,10 +17,12 @@ public class Player : MonoBehaviour
     private bool _hasShot;
     private Vector2 moveVec;
 
+    private Vector3 playerRot;
+
 
     private void Awake()
     {
-       // playerRB = GetComponent<Rigidbody>();
+        playerRB = GetComponent<Rigidbody>();
         controller = new PlayerController();
         controller.Enable();
         moveSpeed = 10f;
@@ -33,7 +36,8 @@ public class Player : MonoBehaviour
         moveVec = controller.Movement.Move.ReadValue<Vector2>();
 
         // playerRB.MovePosition(new Vector3(moveVec.x, 0, moveVec.y) * Time.deltaTime * moveSpeed);
-        transform.position += new Vector3(moveVec.x, 0, moveVec.y) * Time.deltaTime * moveSpeed;
+       // transform.position += new Vector3(moveVec.x, 0, moveVec.y) * Time.deltaTime * moveSpeed;
+        playerRB.velocity = new Vector3(moveVec.x, 0, moveVec.y) * moveSpeed;
         TurnPlayer();
     }
 
@@ -56,25 +60,39 @@ public class Player : MonoBehaviour
         //if w is pressed turn to face up
         if (moveVec.y == 1)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            playerRB.MoveRotation(Quaternion.Euler(0, 0, 0));
         }
 
         //if A is pressed turn to face left
         if (moveVec.x == -1)
         {
-            transform.rotation = Quaternion.Euler(0, -90, 0);
+            playerRB.MoveRotation(Quaternion.Euler(0, -90, 0));
         }
+
+        //If A and W are pressed at the same time
+        if (moveVec.x == -1 & moveVec.y == 1)
+        {
+            playerRB.MoveRotation(Quaternion.Euler(0, 40, 0));
+        }
+
+        //if A and S are pressed at the same time
+        if(moveVec.x == 1 & moveVec.y == -1)
+        {
+            playerRB.MoveRotation(Quaternion.Euler(0, -120, 0));
+        }
+
+        //
 
         //if S is pressed turn to face down
         if (moveVec.y == -1)
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
+            playerRB.MoveRotation(Quaternion.Euler(0, 180, 0));
         }
 
         //if D is pressed turn to face right
         if (moveVec.x == 1)
         {
-            transform.rotation = Quaternion.Euler(0, 90, 0);
+            playerRB.MoveRotation(Quaternion.Euler(0, 90, 0));
         }
     }
     
